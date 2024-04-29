9eea54a38bd2932040133c17cd00d7324614041a
EslamSad3 <eslam.sa3d96@mail.com>
2024-04-29 23:45:49 +0300

    up

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 let navItems = document.querySelectorAll(".nav-item");
 
 navItems.forEach((el) => {
-    el.addEventListener("click", () => {
-        navItems.forEach((item) => {
-            if (item !== el && item.classList.contains("active")) {
-                item.classList.remove("active");
-            }
-        });
-        el.classList.toggle("active");
+  el.addEventListener("click", () => {
+    navItems.forEach((item) => {
+      if (item !== el && item.classList.contains("active")) {
+        item.classList.remove("active");
+      }
     });
+    el.classList.toggle("active");
+  });
 });
 
+let up = document.querySelector(".up");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    up.style.display = "block";
+  } else {
+    up.style.display = "none";
+  }
+});
 
+up.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
